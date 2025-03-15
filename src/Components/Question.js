81ed66cd8d0e77e1a97c5836c8c31f83da684813
Arik59 <kruftik203@gmail.com>
2025-03-15 16:33:57 +0200

    started new project quiz-app: create components

---
+++ src/Components/Question.js
@@ -0,0 +1,2 @@
+import React, {Component} from "react";
+import Options from "./Options"
---
+++ src/Components/QuestionBank.js
@@ -0,0 +1,16 @@
+const qBank = [
+    {
+        id: 1,
+        question: "3 + 2",
+        options: ["3", "4", "5", "6"],
+        answer: "5"
+    },
+    {
+        id: 2,
+        question: "3 - 2",
+        options: ["1", "2", "3", "4"],
+        answer: "1"
+    }
+]
+
+export default qBank;
